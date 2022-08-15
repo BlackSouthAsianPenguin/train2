30f9d1881e1a4673a287b3b9c1843fffa5e03788
BlackSouthAsianPenguin <40156074+BlackSouthAsianPenguin@users.noreply.github.com>
2022-08-15 14:03:28 +0700

    push

--- landingpage/connect.py
+++ landingpage/connect.py
@@ -1,7 +1,7 @@
-import pymysql
+import mysql.connector
 
 #database connection
-connection = pymysql.connect(host="http://188.166.221.246/phpmyadmin",user="training",passwd="training",database="retail_db" )
+connection = mysql.connector(host="http://188.166.221.246/phpmyadmin",user="training",passwd="training",database="retail_db" )
 cursor = connection.cursor()
 # some other statements  with the help of cursor
 connection.close()
