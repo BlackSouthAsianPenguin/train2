423fc9a3ac1ed8d3e8b7c5090b8b6fa002f1e743
BlackSouthAsianPenguin <40156074+BlackSouthAsianPenguin@users.noreply.github.com>
2022-08-15 14:19:21 +0700

    push

--- landingpage/connect.py
+++ landingpage/connect.py
@@ -1,7 +1,13 @@
+# importing required libraries
 import mysql.connector
-
-#database connection
-connection = mysql.connector(host="188.166.221.246",user="training",passwd="training",database="retail_db" )
-cursor = connection.cursor()
-# some other statements  with the help of cursor
-connection.close()
+  
+dataBase = mysql.connector.connect(
+  host ="188.166.221.243",
+  user ="training",
+  passwd ="training"
+)
+ 
+print(dataBase)
+  
+# Disconnecting from the server
+dataBase.close()
